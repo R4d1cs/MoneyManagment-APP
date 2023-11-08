919abb2799ff9d015712c62a3885274cd3540578
Radics Ádám <radicsadam@Radics-MacBook-Air.local>
2023-11-08 22:36:48 +0100

    MON-4 MON-6 MON-8 Frontend, backendben, mappa struktúrában való változtatások voltak.
    Fontend:
     	- Statisztika, a bemutatkozás nézetben.
    	- Szekciókra bonta a bemutatkozás rész, a könnyebb átláthatóság érdekében.
    	- Kisebb igazítás a 'copyright' szövegnél.
    
    Backend:
    	- Lekérdezés megírva, az adott táblából az összes adatot le lehet kérni.
    
    Mappa struktúra:
    	- Külön kapott egy 'Styles' nevű mappát, az adott nézet.

--- frontend/Controllers/index.js
+++ frontend/Controllers/index.js
@@ -1,12 +1,12 @@
-const serverUrl = 'http://localhost:5000';
+const serverUrl = 'http://localhost:3000'
 
 async function render(view) {
-    const main = document.querySelector("main");
-    main.innerHTML = await (await fetch(`Views/${view}.html`)).text();
+    const main = document.querySelector("main")
+    main.innerHTML = await (await fetch(`Views/${view}.html`)).text()
 }
 
 function showMessage(msg){
-    let alertBox = document.querySelector('#alertBox');
-    alertBox.innerHTML = `<strong>Hiba!</strong> ${msg}`;
-    alertBox.classList.remove('d-none');
+    let alertBox = document.querySelector('#alertBox')
+    alertBox.innerHTML = `<strong>Hiba!</strong> ${msg}`
+    alertBox.classList.remove('d-none')
 }
---
+++ frontend/Controllers/introduction.js
@@ -0,0 +1,9 @@
+setTimeout(() => {
+    axios.get(`${serverUrl}/users`).then(res => {
+        memberNum.innerText = res.data.length
+    });
+
+    axios.get(`${serverUrl}/items`).then(res => {
+        dataNum.innerText = res.data.length
+    });
+}, 500)
