5214c62a9345a77a877fcdc7f772546854b0529f
Radics Ádám <radicsadam@Radics-MacBook-Air.local>
2023-11-08 21:19:10 +0100

    MON-6 MON-8 Fronted, backend változtatva.
    Frontend:
    	- Az app átméretezve, illetve háttér megváltoztatva, fejléc átalakítva, a szemnek könnyebben átlátható.
    
    Backend:
    	- A login view azonnali betöltése törölve és az applikáció kinézete rendezve, leegyszerűsítve és letisztítva.

--- frontend/Controllers/index.js
+++ frontend/Controllers/index.js
@@ -1,8 +1,4 @@
-//const serverUrl = 'http://172.16.100.25:5000';
 const serverUrl = 'http://localhost:5000';
-
-let loggedInMenu = document.querySelector('#LoggedInMenu');
-let loggedOutMenu = document.querySelector('#LoggedOutMenu');
 
 async function render(view) {
     const main = document.querySelector("main");
@@ -13,16 +9,4 @@ function showMessage(msg){
     let alertBox = document.querySelector('#alertBox');
     alertBox.innerHTML = `<strong>Hiba!</strong> ${msg}`;
     alertBox.classList.remove('d-none');
-}
-
-loggedUser = JSON.parse(sessionStorage.getItem('stepCounterUser'));
-
-if (loggedUser != null){
-    loggedInMenu.classList.remove('d-none');
-    loggedOutMenu.classList.add('d-none');
-    render('newdata');
-}else{
-    loggedInMenu.classList.add('d-none');
-    loggedOutMenu.classList.remove('d-none');
-    render('login');
 }
