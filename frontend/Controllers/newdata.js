0c45948714a8cd3dddfd1c3dfaf56be6f3ccc77e
Radics Ádám <radicsadam@Radics-MacBook-Air.local>
2023-11-09 21:31:18 +0100

    MON-7 Új adat rögzítés készen van, beviteli mezők ellenőrzésével együtt.

---
+++ frontend/Controllers/newdata.js
@@ -0,0 +1,58 @@
+function newDataButtonCheck() {
+    let newDataButton = document.querySelector('#newdataButton')
+    let newDate = document.querySelector('#newdataDate')
+    let newType = document.querySelector('#newdataType')
+    let newAmount = document.querySelector('#newdataAmount')
+    let newTag = document.querySelector('#newdataTag')
+    
+    if ((newDate.value && newType.value && newAmount.value && newTag.value) == '') { 
+        newDataButton.disabled = true
+        return
+    }
+
+    newDataButton.disabled = false
+}
+
+function addNewData() {
+    let newDate = document.querySelector('#newdataDate')
+    let newType = document.querySelector('#newdataType')
+    let newAmount = document.querySelector('#newdataAmount')
+    let newTag = document.querySelector('#newdataTag')
+
+    axios.get(`${serverUrl}/items/userID/eq/${loggedUser.ID}`).then(res => {
+        let newData = {
+            userID: loggedUser.ID, 	
+            date: newDate.value, 	
+            type: newType.value,
+            amount: newAmount.value,
+            tag: newTag.value
+        }
+    
+        axios.post(`${serverUrl}/items`, newData).then(()=>{
+            showMessage('success', 'Új adat rögzítve az adott dátumra!', 5)
+        });
+
+        newDate.value = ''
+        newType.value = ''
+        newAmount.value = ''
+        newTag.value = ''
+    })
+}
+
+function getToday() {
+    var today = new Date()
+    var dd = today.getDate()
+    var mm = today.getMonth() + 1
+    var yyyy = today.getFullYear()
+    
+    if (dd < 10) {
+       dd = '0' + dd
+    }
+    
+    if (mm < 10) {
+       mm = '0' + mm
+    } 
+        
+    today = yyyy + '-' + mm + '-' + dd
+    return today
+}
